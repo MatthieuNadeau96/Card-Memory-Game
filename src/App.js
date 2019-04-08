03bf43910f90c8d11263d69816e8605f3314497d
Matthieu <matthieunadeau96@gmail.com>
2019-04-08 13:57:07 -0400

    Added Card component and started visualizing the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Card from './Card';
+
 class App extends Component {
   render() {
     return (
-      <div>
-
+      <div className="game-container">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
       </div>
     );
   }
---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+
+  render() {
+    return (
+      <div className="card">
+        <div className="card-front">Front</div>
+      </div>
+    );
+  }
+
+}
+
+export default Card;
