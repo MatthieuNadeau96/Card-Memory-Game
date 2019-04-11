7b89db6ef08bc586c1a94131e03aaca7dc31bc7e
Matthieu <matthieunadeau96@gmail.com>
2019-04-11 18:51:56 -0400

    added images to the cards and added a back&front face

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 
 import Card from './Card';
 
+import atom from './assets/imgs/atom.png';
+import cloud from './assets/imgs/cloud.png';
+import heart from './assets/imgs/like.png';
+import moon from './assets/imgs/moon-phase-outline.png';
+import pawprint from './assets/imgs/pawprint.png';
+import star from './assets/imgs/star.png';
+import sun from './assets/imgs/sun.png';
+
 class App extends Component {
 
   state = {
@@ -10,50 +18,74 @@ class App extends Component {
       1: {
         face: 1,
         flipped: false,
+        frontImage: atom,
+        backImage: cloud,
       },
       2: {
         face: 2,
         flipped: false,
+        frontImage: atom,
+        backImage: cloud,
       },
       3: {
         face: 3,
         flipped: false,
+        frontImage: atom,
+        backImage: heart,
       },
       4: {
         face: 4,
         flipped: false,
+        frontImage: atom,
+        backImage: heart,
       },
       5: {
         face: 5,
         flipped: false,
+        frontImage: atom,
+        backImage: moon,
       },
       6: {
         face: 6,
         flipped: false,
+        frontImage: atom,
+        backImage: moon,
       },
       7: {
         face: 7,
         flipped: false,
+        frontImage: atom,
+        backImage: pawprint,
       },
       8: {
         face: 8,
         flipped: false,
+        frontImage: atom,
+        backImage: pawprint,
       },
       9: {
         face: 9,
         flipped: false,
+        frontImage: atom,
+        backImage: star,
       },
       10: {
         face: 10,
         flipped: false,
+        frontImage: atom,
+        backImage: star,
       },
       11: {
         face: 11,
         flipped: false,
+        frontImage: atom,
+        backImage: sun,
       },
       12: {
         face: 12,
         flipped: false,
+        frontImage: atom,
+        backImage: sun,
       },
     },
   }
@@ -75,6 +107,8 @@ class App extends Component {
             <Card
               key={index}
               face={cards[card].face}
+              frontImage={cards[card].frontImage}
+              backImage={cards[card].backImage}
               clicked={() => this.flipCard(card)}
               class={this.state.cards[card].flipped ? 'card flipped' : 'card'}
             />
--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,8 @@ class Card extends Component {
   render() {
     return (
       <div className={this.props.class} onClick={this.props.clicked}>
-        <div>{this.props.face}</div>
+        <img className="back-face" src={this.props.backImage}/>
+        <img className="front-face" src={this.props.frontImage}/>
       </div>
     );
   }
