de34607ed670a28a41b37994d0480a7c88e47f6e
Matthieu <matthieunadeau96@gmail.com>
2019-04-21 12:51:07 -0400

    Updates

---
+++ precache-manifest.305a969ef587ea4c895a051e8c9fc799.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c814a9fdce91443568d",
+    "url": "/Card-Memory-Game/static/css/main.1b6930fc.chunk.css"
+  },
+  {
+    "revision": "8c814a9fdce91443568d",
+    "url": "/Card-Memory-Game/static/js/main.d05ecc66.chunk.js"
+  },
+  {
+    "revision": "1c7e90ae58eeb1b1a85f",
+    "url": "/Card-Memory-Game/static/js/runtime~main.69c5030a.js"
+  },
+  {
+    "revision": "74cea2fd69fd6eb52fb7",
+    "url": "/Card-Memory-Game/static/js/2.63cf11a9.chunk.js"
+  },
+  {
+    "revision": "04a2737ec7685b962eaac3a0b8c3efe9",
+    "url": "/Card-Memory-Game/static/media/atom.04a2737e.png"
+  },
+  {
+    "revision": "48ce5f432c68e076eeb13fb055ffbc3d",
+    "url": "/Card-Memory-Game/static/media/cloud.48ce5f43.png"
+  },
+  {
+    "revision": "5a6c188d39356c50c2f6eeeaddce3560",
+    "url": "/Card-Memory-Game/static/media/like.5a6c188d.png"
+  },
+  {
+    "revision": "adf3581c7f289c6a9c30216bff4e3c5c",
+    "url": "/Card-Memory-Game/static/media/moon-phase-outline.adf3581c.png"
+  },
+  {
+    "revision": "e8521fe5b5ac98dbab14b3eef846e3a0",
+    "url": "/Card-Memory-Game/static/media/pawprint.e8521fe5.png"
+  },
+  {
+    "revision": "054ad0146841dcafaa5ccbfcbdc36e63",
+    "url": "/Card-Memory-Game/static/media/star.054ad014.png"
+  },
+  {
+    "revision": "339889260480996e687ac65f7e3144dd",
+    "url": "/Card-Memory-Game/index.html"
+  }
+];
